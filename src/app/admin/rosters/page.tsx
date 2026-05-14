432715989fbb3f0b1e4e600c977db2b13ac29d83
Bryan Fennell <bryanandrewfennell@gmail.com>
2026-05-13 22:44:27 -0400

    error fix

--- src/app/admin/rosters/page.tsx
+++ src/app/admin/rosters/page.tsx
@@ -26,7 +26,8 @@ export default async function AdminRostersPage() {
         conferences={conferences ?? []}
         teams={teams ?? []}
         pokemon={pokemon ?? []}
-        roster={roster ?? []}
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        roster={(roster ?? []) as any}
       />
     </div>
   );
